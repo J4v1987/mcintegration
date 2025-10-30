26f10a73cd13f84bece36a7905a94796622ec04d
Javier Romo-Leon <j.arl@outlook.com>
2025-10-30 12:08:34 +0100

    Amended comments style

--- MCIntegration.cpp
+++ MCIntegration.cpp
@@ -22,30 +22,30 @@ Based on the works of:
     Nantes, France.
 */
 
-# include <stdlib.h>                            // in c for using drand48 ()
-# include <stdio.h>                             // for using printf
-# include <math.h>                              // for using math functions
-int main(){                                     // start of program
-int i,N;                                        // index, num of events
-double x,p,fx,wx,fw,sum,sum2,err;               // values of x,y f (x)...
+# include <stdlib.h>                                // in c for using drand48 ()
+# include <stdio.h>                                 // for using printf
+# include <math.h>                                  // for using math functions
+int main(){                                         // start of program
+int i,N;                                            // index, num of events
+double x,p,fx,wx,fw,sum,sum2,err;                   // values of x,y f (x)...
 printf("Enter number of N events:");
-scanf ("%d",&N);                                // do not forget the pointer of scanf
-printf ("\nCalculation using %d events\n",N);   // statistics
-sum=0; sum2=0;                                  // initialize sum and sum2
+scanf ("%d",&N);                                    // do not forget the pointer of scanf
+printf ("\nCalculation using %d events\n",N);       // statistics
+sum=0; sum2=0;                                      // initialize sum and sum2
 
-for (i=0; i<N; i++) {                           // loop on event number
-    p=drand48();                                // random number in (0,1)
-    x=pow(p,0.2);                               // INSERT HERE x(p)
-    fx=pow(x,2)*pow(sin(x),2);                  // INSERT HERE f(x)
-    wx=5*pow(x,4);                              // INSERT HERE w(x)
-    fw=fx/wx;                                   // just the quotient
-    sum+=fw;                                    // sum up f/w
-    sum2+=fw*fw;                                // sum up (f/w) **2
-};                                              // end of loop
-sum/=N;                                         // average <f/w>
-sum2/=N;                                        // average <(f/w) **2>
-err=sqrt((sum2-sum*sum)/N);                     // theoretical precision
+for (i=0; i<N; i++) {                               // loop on event number
+    p=drand48();                                    // random number in (0,1)
+    x=pow(p,0.2);                                   // INSERT HERE x(p)
+    fx=pow(x,2)*pow(sin(x),2);                      // INSERT HERE f(x)
+    wx=5*pow(x,4);                                  // INSERT HERE w(x)
+    fw=fx/wx;                                       // just the quotient
+    sum+=fw;                                        // sum up f/w
+    sum2+=fw*fw;                                    // sum up (f/w) **2
+};                                                  // end of loop
+sum/=N;                                             // average <f/w>
+sum2/=N;                                            // average <(f/w) **2>
+err=sqrt((sum2-sum*sum)/N);                         // theoretical precision
 
-printf ("Result %f +- %f along [0,1]\n",sum, err);          // print results
+printf ("Result %f +- %f along [0,1]\n",sum, err);  // print results
 printf ("est. result %f along [-1,1]\n",2*sum);
-};                                              // end of the program
+};                                                  // end of the program
